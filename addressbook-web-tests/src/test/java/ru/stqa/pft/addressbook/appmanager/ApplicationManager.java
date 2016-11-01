7e7f88ac1cd771b40fb0c91c980bfcee2342fc4d
Anettc <Anettc@mail.ru>
2016-11-01 23:26:41 +0300

    Общий базовый класс Base Helper для классов помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -12,18 +10,10 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
   FirefoxDriver wd;
-private SessionHelper sessionHelper;
+  private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   public void init() {
     wd = new FirefoxDriver();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,14 +6,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Work on 01.11.2016.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+
+    super(wd);
   }
 
   public void gotoGrouPage() {
-    wd.findElement(By.linkText("groups")).click();
+
+    click(By.linkText("groups"));
   }
 }
