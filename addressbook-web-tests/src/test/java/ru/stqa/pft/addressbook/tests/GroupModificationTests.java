c80f5de8bf244f699ee783f748c3a3f97cdc0c8f
Anettc <Anettc@mail.ru>
2016-11-01 23:43:54 +0300

    Создан тест без использования рекордера

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by Work on 01.11.2016.
+ */
+public class GroupModificationTests extends TestBase {
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGrouPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
