3029b21589f21e14a365ad916983b88e61a96796
Anettc <Anettc@mail.ru>
2016-11-06 14:17:19 +0300

    Заполнение форм с почти одинаковым набором полей2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,8 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 /**
@@ -10,40 +12,32 @@ import ru.stqa.pft.addressbook.model.ContactData;
  */
 public class ContactHelper  extends HelperBase{
 
-  public ContactHelper(WebDriver wd) {
-    super(wd);
-
-  }
-
-  public void submitContactCreation() {
-
-    click(By.xpath("//div[@id='content']/form/input[21]"));
-  }
-
-  public void deleteSelectedContact() {
-
-    click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
-    //click(By.xpath("//*[@id='content']/input[@value='Delete']"));
-
-  }
-
-  public void selectContact() {
-
-    click(By.name("selected[]"));
-  }
-
-  public void returnToHomePage() {
-
-    click(By.linkText("home"));
-  }
+  public ContactHelper(WebDriver wd) { super(wd); }
 
 
+  public void deleteSelectedContact() { click(By.xpath("//div[@id='content']/form[2]/div[2]/input")); }
 
-  public void submitContactModification() {
-    click(By.name("update"));
-  }
+  public void selectContact() { click(By.name("selected[]")); }
 
   public void initContactModification() {
     click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
   }
+
+  public void fillContactForm(ContactData contactData, boolean creation) {
+    type(By.name("firstname"), contactData.getFirstname());
+    type(By.name("lastname"),contactData.getLastname());
+    type(By.name("nickname"),contactData.getNickname());
+    type(By.name("home"),contactData.getHome());
+    type(By.name("mobile"),contactData.getMobile());
+
+    if (creation) {
+      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+    } else  {
+      Assert.assertFalse(isElementPresent(By.name("new_group")));
+    }
+  }
+  public void submitContactCreation() { click(By.xpath("//div[@id='content']/form/input[21]"));}
+
+  public void returnToHomePage() { click(By.linkText("home"));}
+
 }
