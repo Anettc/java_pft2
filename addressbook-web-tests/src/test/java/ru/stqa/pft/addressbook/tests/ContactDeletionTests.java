f8e827b182c65ce6a5fb565645feabcbfeba03ec
Anettc <Anettc@mail.ru>
2016-11-15 16:56:31 +0300

    Выбор элемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -13,7 +13,7 @@ public class ContactDeletionTests extends TestBase {
  if (! app.getContactHelper().isThereAContact()){
  app.getContactHelper().createContact(new ContactData("firstname", "lastname", "nickname", "home", "mobile", "test1"));
     }
-    app.getContactHelper().selectContact();
+    app.getContactHelper().selectContact(before - 1);
     app.getContactHelper().deleteSelectedContact();
     app.getContactHelper().agreeContactDeletion();
     app.getContactHelper().returnToHomePage();
