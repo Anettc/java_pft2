b5f1f713120577c21c6f5fc9d14e7fa462491991
Anettc <Anettc@mail.ru>
2016-12-20 23:00:53 +0300

    Формат XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -23,6 +24,9 @@ public class GroupDataGenerator {
     @Parameter (names = "-f" , description = "Target file" )
     public String file;
 
+    @Parameter (names = "-d" , description = "Data format" )
+    public String format;
+
 
 public static void main (String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
@@ -41,15 +45,35 @@ public static void main (String[] args) throws IOException {
 
     private void run() throws IOException {
         List<GroupData> groups = generatsGroups(count);
+        if (format.equals("csv")){
+            saveAsCsv(groups,new File(file));
+        } else if(format.equals("xml")){
+                saveAsXml(groups,new File(file));
+            } else {
+            System.out.println("Unrecognized format" + format);
+        }
         save(groups, new File(file));
 
+    }
+
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+           xstream.processAnnotations(GroupData.class);
+            String xml = xstream.toXML(groups);
+            Writer writer = new FileWriter (file);
+            writer.write(xml);
+            writer.close();
+    }
+
+    private void saveAsCsv(List<GroupData> groups, File file) {
+        System.out.println(new File(".").getAbsolutePath());
     }
 
     private void save(List<GroupData> groups, File file) throws IOException{
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups ){
-            writer.write(String.format("%s;&s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+            writer.write(String.format("%s; %s; %s \n", group.getName(), group.getHeader(), group.getFooter()));
 
         }
         writer.close();
@@ -59,7 +83,7 @@ public static void main (String[] args) throws IOException {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i=0; i<count; i++){
         groups.add(new GroupData().withName(String.format("test %s", i))
-                .withHeader(String.format("header %s", i)).withFooter(String.format("footer &s", i)));
+                .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
     }
         return groups;
     }
